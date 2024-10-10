c61418e7b397d98d001a2f27dcf0018f69c315c2
Pietro Pacanaro <pietro.macho12@gmail.com>
2024-10-09 21:02:59 -0300

    prop, event handlers, hooks, state and conditional content

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import reactImg from './assets/react-core-concepts.png';
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const description = reactDescriptions[getRandomInt(2)]
-
-  return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -53,6 +33,27 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onClick={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onClick={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (<div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div> 
+          )} 
         </section>
       </main>
     </div>
